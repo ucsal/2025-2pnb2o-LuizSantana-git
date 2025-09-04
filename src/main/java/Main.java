10e3c15e3233e27e46d971f63d20c97cbac7652a
Luiz Alberto Guerreiro Soares Santana <luizguerreirosantana@gmail.com>
2025-09-04 14:35:03 -0300

    DiscountCalculator com injeção de DiscountPolicy e construtor default, respeitando o OCP.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,6 @@ public class Main {
 		DiscountCalculator calc = new DiscountCalculator();
 		System.out.println("REGULAR 100 -> " + calc.apply(100, CustomerType.REGULAR));
 		System.out.println("PREMIUM 100 -> " + calc.apply(100, CustomerType.PREMIUM));
-		// PARTNER deveria ter 12%
 		System.out.println("PARTNER 100 -> " + calc.apply(100, CustomerType.PARTNER));
 	}
 }
--- src/main/java/br/com/mariojp/solid/ocp/DiscountCalculator.java
+++ src/main/java/br/com/mariojp/solid/ocp/DiscountCalculator.java
@@ -1,11 +1,33 @@
 package br.com.mariojp.solid.ocp;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class DiscountCalculator {
-    public double apply(double amount, CustomerType type){
-        switch (type){
-            case REGULAR: return amount * 0.95;
-            case PREMIUM: return amount * 0.90;
-            default: return amount;
+
+    private final Map<CustomerType, DiscountPolicy> policies;
+    //mapa de policies
+
+    public DiscountCalculator(Map<CustomerType, DiscountPolicy> policies) {
+        this.policies = policies;
+        //construtor que inveta dependencias
+    }
+
+    //construtor com padrões
+    public DiscountCalculator() {
+        Map<CustomerType, DiscountPolicy> defaults = new HashMap<>();
+        defaults.put(CustomerType.REGULAR, new RegularPolicy());
+        defaults.put(CustomerType.PREMIUM, new PremiumPolicy());
+        defaults.put(CustomerType.PARTNER, new PartnerPolicy());
+        this.policies = defaults;
+    }
+
+    public double apply(double amount, CustomerType type) {
+
+        if (this.policies.containsKey(type)) {
+            return this.policies.get(type).apply(amount);
+        } else {
+            return amount;
         }
     }
 }
